f8d6c638b2b53f78f1dd69471b74edeb5a4fa354
ysato <s171039@stn.nagaokaut.ac.jp>
2022-01-10 13:26:09 +0900

    投稿機能の追加

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import generic
+
+from .models import Post
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(generic.TemplateView):
     template_name = 'blog/index.html'
+
+class PostListView(generic.ListView):
+    template_name = 'blog/post_list.html'
+    model = Post
